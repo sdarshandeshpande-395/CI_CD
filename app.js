2819025dcedc4faf903bdffb417bd73e6f82bdde
Ubuntu <ubuntu@ip-172-31-47-108.ap-south-1.compute.internal>
2026-03-20 07:16:03 +0000

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.write("Hello from CI/CD Pipeline 🚀");
+  res.write("Hello from CI/CD Pipeline");
+  res.wite("This is Sudarshan Deshpande");
   res.end();
 }).listen(3000);
